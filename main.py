d3e1407c2560b0cbb254657c9fd0246bf6797137
Kohei Wada <program3152019@gmail.com>
2023-03-31 03:15:13 +0900

    updated

--- main.py
+++ main.py
@@ -7,16 +7,63 @@ import atexit
 # Define OpenAI API key 
 openai.api_key = os.environ['APIKEY_GPT3']
 
-# Set up the model and prompt
-model_engine = "text-davinci-003"
-
 GREEN = '\033[32m'
 END   = '\033[0m'
 console = GREEN + '<chatGPT>' + END
 
 historyfile = os.path.join(os.path.expanduser("~"), ".chatGPT_history")
 
-def interactive_loop():
+def chat(conversationHistory):
+    response = openai.ChatCompletion.create(
+        messages=conversationHistory,
+        max_tokens=1024,
+        n=1,
+        stream=True,
+        temperature=0.5,
+        stop=None,
+        presence_penalty=0.5,
+        frequency_penalty=0.5,
+        model="gpt-3.5-turbo"
+    )
+
+    fullResponse = ""
+    for chunk in response:
+        text = chunk['choices'][0]['delta'].get('content')
+
+        if text is not None:
+            fullResponse += text
+            print(text, end='', flush=True) 
+
+    print('')
+    return fullResponse
+
+
+def main_loop(): 
+    conversationHistory = []
+    while True:
+        try:
+            text = input(console)
+        except EOFError:
+            break
+        except KeyboardInterrupt:
+            print("^C")
+            continue
+        if not text: 
+            continue
+
+        user_action = {"role": "user", "content": text}
+        conversationHistory.append(user_action)
+        
+        try:
+            res = chat(conversationHistory)
+            chatGPT_responce = {"role": "assistant", "content": res}
+            conversationHistory.append(chatGPT_responce) 
+        except:
+            print('')
+            pass
+
+
+def interactive_shell():
     try:
         readline.read_init_file()
     except:
@@ -26,37 +73,15 @@ def interactive_loop():
     except FileNotFoundError:
         open(historyfile, 'wb').close()
 
-    while True:
-        try:
-            chat = input(console)
-        except EOFError:
-            break
-        except KeyboardInterrupt:
-            print("^C")
-            continue
-
-        if not chat: 
-            continue
-        try:
-            completion = openai.Completion.create(
-                engine=model_engine,
-                prompt=chat,
-                max_tokens=1024,
-                n=1,
-                stop=None,
-                temperature=0.5,
-            )
-        except BaseException as e:
-            break
-
-        response = completion.choices[0].text
-        print(response)
+    main_loop()
 
     print("Byebye!")
     atexit.register(readline.write_history_file, historyfile)
 
+
 def main(): 
-    interactive_loop()
+    interactive_shell()
+
 
 if __name__ == "__main__": 
     main() 
